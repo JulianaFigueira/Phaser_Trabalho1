46e3b9526425b10d1d32f2967eae395cd5955514
Manoel da Silva Neto <manoel180@hotmail.com>
2017-05-12 02:47:54 -0400

    ajustando o botao

--- js/home.js
+++ js/home.js
@@ -9,24 +9,36 @@
 //Um estado é sempre um objeto JavaScript, com no mínimo as 3 funções principais: preload, create e update
 //As funções sempre começam com NomeDoObjeto.prototype 
 var HomeState = function(game) {};
+var button;
 
 // preload: carregar todos os assets necessários para esta scene ou para as próximas
 HomeState.prototype.preload = function() {
     // Não há nenhum asset a ser carregado aqui, então a função fica vazia
       this.game.load.image('play', 'Assets/play.png');
+    this.game.load.spritesheet('button', 'assets/buttons/button_sprite_sheet.png', 193, 71);
    
 }
 
 // create: instanciar e inicializar todos os objetos dessa scene
 HomeState.prototype.create = function() {
     game.add.image(game.world.centerX, game.world.centerY, 'play').anchor.set(0.5);
-    this.game.add.text(170, 500, "home + credits page (play = spacebar)", {fill: "#ffffff"});
     this.PlayKey = this.game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);
-  
+    this.button = game.add.button(game.world.centerX - 70, 450, 'button', actionOnClick, this, 0,0,0 );
+    this.button.width = 155;
+    this.button.height = 40;
+     //this.button.anchor.setTo(0.5, 0.5);
+    this.button = new LabelButton(this.game, 480, 512, "button", "Start game!", this.doBtnStartHandler, this, 0, 0, 0); // button frames 1=over, 0=off, 2=down
+
 }
 
 // update: o que fazer a cada quadro por segundo
 HomeState.prototype.update = function() {
     if(this.PlayKey.isDown)
         this.game.state.start('game');
+}
+
+function actionOnClick () {
+
+    this.game.state.start('game');
+
 }
